e28b9f2b335392d3d91b14ff5276fc5698769ad4
Robin Joseph <robinjoseph08@sbcglobal.net>
2014-02-28 10:40:11 -0600

    flush out test example

--- test/test.js
+++ test/test.js
@@ -1,23 +1,47 @@
 var bs = require('../lib/node-beanstalkd')
   , client = new bs.Client();
 
-client.connect('127.0.0.1:11300', function(err, conn) {
-  var job_data = {"data": {"name": "node-beanstalk-client"}};
-  conn.put(0, 0, 1, JSON.stringify(job_data), function(err, job_id) {
-    console.log('put job: ' + job_id);
-
-    conn.reserve(function(err, job_id, job_json) {
-      console.log('got job: ' + job_id);
-      console.log('got job data: ' + job_json);
-      console.log('module name is ' + JSON.parse(job_json).data.name);
-      conn.destroy(job_id, function(err) {
-	      console.log('destroyed job');
-      });
+client.connect('127.0.0.1:11300', function (err, conn) {
+  if(err) {
+    console.log('Error connecting to beanstalkd.');
+    console.log('Make sure that beanstalkd is running.');
+  } else {
+    var data = {
+      data: {
+        name: "node-beanstalkd"
+      }
+    };
+    conn.put(0, 0, 1, JSON.stringify(data), function (err, id) {
+      if(err) {
+        console.log('Error putting job.');
+      } else {
+        console.log('Produced Job ' + id);
+        conn.reserve(function (err, id, json) {
+          if(err) {
+            console.log('Error reserving job.');
+          } else {
+            console.log('Consumed Job ' + id);
+            console.log('Job Data: ' + json);
+            console.log('Name: ' + JSON.parse(json).data.name);
+            conn.destroy(id, function (err) {
+              if(err) {
+                console.log('Error destroying job.');
+              } else {
+                console.log('Destroyed Job');
+                conn.end();
+              }
+            });
+          }
+        });
+      }
     });
-
-  });
+  }
 });
 
-client.on('close', function (err) {
-  console.log('closing...');
+client.on('close', function (has_err) {
+  if(has_err) {
+    console.log('Connection closed with an error.');
+  } else {
+    console.log('Connection was closed successfully.');
+  }
 });
